c3a9f83c0b7faaf96c2048af963443829e0adc5d
Moon1x21 <moonjooeun4741@gmail.com>
2020-06-02 00:45:09 +0900

    기초작업

---
+++ Desktop/dancer_app/src/pages/homePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './NavBar';
+
+function Home() {
+  return (
+    <div>
+        <Menu/>
+        home
+    </div>
+  )
+}
+
+export default Home;
---
+++ Desktop/dancer_app/src/pages/mapPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Map() {
+  return (
+    <div>
+      map
+    </div>
+  )
+}
+
+export default Map;
+
+
